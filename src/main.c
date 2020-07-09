be9ef0a56e367f8d28e1c6da6bae0e25282a848f
clissiabovi <c195744@dac.unicamp.br>
2020-07-09 17:44:05 -0300

    5/5

--- src/main.c
+++ src/main.c
@@ -1,17 +1,62 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define MAX 20
+
+// numero de primos e numero de threads simultaneas
+int ndp, ndt;
+
+void *primo (void* n){
+
+	int *numero;
+	int i, j=0;
+	numero=(int*) n;
+	for(i=2; i<(*numero); i++){
+		if((*numero)%i==0){
+			ndt--;
+			return NULL;
+		}
+	}
+	if(j==0 && (*numero)>1){
+		ndp++;
+	}
+	ndt--;	
+	return NULL;
+}
 
 int main() {
+	pthread_t threads[MAX];
+	int ndv=0, i;
+	int *valores;
+	char c='\0';
+	ndp=0;ndp=0;
+	valores=(int*) malloc(MAX*sizeof(int));
 
-  int x, y;
-
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+// lendo os numeros e descobrindo quantos sao
+	while(c!='\n'){
+		scanf("%d%c", &valores[ndv], &c);
+		ndv++;
+	}
+// criando as threads
+	for(i=0; i<ndv; i++){
+		while(ndt>=4){
+		}
+		ndt++;
+		pthread_create((&threads[i]), NULL, primo, (&valores[i]));
+	}
+// esperando as threads terminarem	
+	for(i=0; i<ndv; i++){
+		pthread_join(threads[i], NULL);
+	}
+	printf("%d\n", ndp);  	  	
+ 	free(valores);
+	return 0;
 }
+
+
+
+
+
+
+
